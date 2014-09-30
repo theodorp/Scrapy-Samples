2ccdb4c7f99e6ffd7db1b3f5b4f6853cc3565461
Theodor Perivolaris <theodor.perivolaris@gmail.com>
2014-09-30 00:51:07 -0400

    Update 30/09/14 - works w/ CL now
    Changed allow= regex, and start_url to work with CL. Only does first 900 listings.

--- crawlspider/craigslist_sample/spiders/test2.py
+++ crawlspider/craigslist_sample/spiders/test2.py
@@ -6,15 +6,15 @@ from craigslist_sample.items import CraigslistSampleItem
 class MySpider(CrawlSpider):
     name = "craigs"
     allowed_domains = ["sfbay.craigslist.org"]
-    start_urls = ["http://sfbay.craigslist.org/npo/"]   
+    start_urls = ["http://sfbay.craigslist.org/search/npo?"]   
 
-    rules = (Rule (SgmlLinkExtractor(allow=("index\d00\.html", ),restrict_xpaths=('//a[@class="button next"]',))
+    rules = (Rule (SgmlLinkExtractor(allow=("/?s=\d00\&", ),restrict_xpaths=('//a[@class="button next"]',))
     , callback="parse_items", follow= True),
     )
 
     def parse_items(self, response):
         hxs = HtmlXPathSelector(response)
-        titles = hxs.select('//span[@class="pl"]')
+        titles = hxs.select("//span[@class='pl']")
         items = []
         for titles in titles:
             item = CraigslistSampleItem()
